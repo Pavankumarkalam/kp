31c9603ecbb6326c4299d647ee92d6a9171ee7ff
Pavankumarkalam <kalampavan453@gmail.com>
2025-07-27 20:06:38 +0530

    Delete shoppee directory

--- shoppee/Start_ShoppeHV.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting ShoppeHV Application - Turquoise Theme...
-echo.
-echo Access the application at: http://localhost:8000
-echo.
-echo Press Ctrl+C to stop the server when done
-cd "%~dp0"
-python -m http.server 8000
-pause
